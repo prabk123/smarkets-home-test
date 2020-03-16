434ce905c8c23b5e4478cd5446680b4fd9275086
Prabodh Kakodkar <prabodhkakodkar@prabodhs-mbp.home>
2020-03-16 12:43:27 +0000

    refactor proxy code

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,9 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    "/v3",
-    proxy({ target: "https://api.smarkets.com", changeOrigin: true })
-  );
-  app.use(
-    "/v0",
-    proxy({ target: "https://api.smarkets.com", changeOrigin: true })
+    proxy(["/v3/**", "/v0/**"], {
+      target: "https://api.smarkets.com",
+      changeOrigin: true
+    })
   );
 };
